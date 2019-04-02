74e45bfd3287d799040fe41b4ba424b4e54f995f
Carlo Jeske <jeske@sc-networks.com>
2019-04-02 11:02:58 +0200

    [ts] added declarationfiles

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import plusnew, { store, Component, ApplicationElement, Props } from 'plusnew';
+import plusnew, { store, Component, ApplicationElement, Props, storeType, ComponentContainer } from 'plusnew';
 
 type position = {
   x: number;
@@ -55,7 +55,7 @@ function renderProps<T>(props: T): T {
   return (props as any)[0];
 }
 
-export default function <T>() {
+export default function <T>(): {store: storeType<dragState<T>,actions<T>>, Component: ComponentContainer<dragProps<T>>} {
   const inactiveDrag: dragState<T> = { active: false };
   const dragStore = store(inactiveDrag as dragState<T>, (state, action: actions<T>) => {
     switch (action.type) {
--- src/test.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentContainer } from 'plusnew';
-
-export function componentPartial<P>(component: ComponentContainer<P>) {
-  const componentpartial = component as ComponentContainer<Partial<P>>;
-  return componentpartial;
-}
